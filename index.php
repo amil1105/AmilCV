fddd9c767f37bcf02c12643df6adcdef2e4e08da
amil1105 <51198499+amil1105@users.noreply.github.com>
2023-05-13 23:07:05 +0400

    Bir Takım Tasarım Değişiklileri

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Start Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Start</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="contact.php">Contact</a></li>
+				<li><a href="login.php">Login</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<h1>Welcome to my website!</h1>
+		<p>This is the start page.</p>
+	</main>
+	<footer>
+		<p>&copy; 2023 My Website</p>
+	</footer>
+</body>
+</html>
